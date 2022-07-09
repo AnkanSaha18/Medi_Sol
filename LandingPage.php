f9100d171e00ad7d42415774369554ccd96812f4
AnkanSaha18 <95054004+AnkanSaha18@users.noreply.github.com>
2022-07-09 22:02:00 +0600

    Landing Page Design is completed without proper Linking

--- LandingPage.php
+++ LandingPage.php
@@ -2,10 +2,8 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-if(isset($_SESSION['loginTime']))
-{
-    if(time()-$_SESSION['loginTime']>3600)
-    {
+if (isset($_SESSION['loginTime'])) {
+    if (time() - $_SESSION['loginTime'] > 3600) {
         unset($_SESSION['username']);
     }
 }
@@ -22,9 +20,7 @@ if(isset($_SESSION['loginTime']))
     <title>Home</title>
 
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-        rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/LandingPage.css">
     <!-- For Font Awesome -->
     <link rel="stylesheet" href="../FontAwesome/css/all.css">
@@ -38,20 +34,16 @@ if(isset($_SESSION['loginTime']))
                 <p style="display: inline; font-weight: bolder; font-size: large;"> Medi Sol</p>
             </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span
-                    class="navbar-toggler-icon"></span></button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav m-auto">
                     <li class="nav-item"><a href="#" class="nav-link" style="margin-left: 20px;">Home</a></li>
                     <li class="nav-item dropdown" style="margin-left: 20px;">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Services
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown"
-                            style="background-color: rgb(60, 138, 255); color: white;">
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: rgb(60, 138, 255); color: white;">
                             <a class="dropdown-item" href="#">Doctor</a>
                             <a class="dropdown-item" href="#">Hospital</a>
                             <a class="dropdown-item" href="#">Ambulance</a>
@@ -64,14 +56,14 @@ if(isset($_SESSION['loginTime']))
                 </ul>
                 <ul class="navbar-nav mr-auto">
                     <li class="navbar-item"><a href="Signup_Signin.php" class="nav-link" style="margin: 0px 50px;"><u>
-                        <!-- Login -->
-                        <?php
-                        if(isset($_SESSION['username']))
-                        echo $_SESSION['username'];
-                        else 
-                        echo "Login";
-                        ?>
-                    </u></a></li>
+                                <!-- Login -->
+                                <?php
+                                if (isset($_SESSION['username']))
+                                    echo $_SESSION['username'];
+                                else
+                                    echo "Login";
+                                ?>
+                            </u></a></li>
                 </ul>
             </div>
         </nav>
@@ -152,13 +144,176 @@ if(isset($_SESSION['loginTime']))
 
 
 
+    <main class="container">
+        <div class="row row-cols-3 justify-content-center">
+            <div class="col section-header">
+                <h5>Browse Services</h5>
+                <hr>
+            </div>
+        </div>
+        <div class="row row-cols-lg-3 row-cols-md-2  row-cols-sm-1 justify-content-center">
+            <div class="col">
+                <div class="service">
+                    <img src="Image/doctor1.jpg" alt="doctor"><br><br>
+                    <h5>Doctor</h5>
+                </div>
+            </div>
+            <div class="col">
+                <div class="service">
+                    <img src="Image/hospital1.jpg" alt="doctor"><br><br>
+                    <h5>Hospital</h5>
+                </div>
+            </div>
+            <div class="col">
+                <div class="service">
+                    <img src="Image/ambulance1.png" alt="doctor"><br><br>
+                    <h5>Ambulance</h5>
+                </div>
+            </div>
+            <div class="col">
+                <div class="service">
+                    <img src="Image/pharmacy1.jpg" alt="doctor"><br><br>
+                    <h5>Pharmacy</h5>
+                </div>
+            </div>
+            <div class="col">
+                <div class="service">
+                    <img src="Image/blood_bank1.webp" alt="doctor"><br><br>
+                    <h5>Blood Bank</h5>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row row-cols-3 justify-content-center">
+            <div class="col section-header">
+                <h5>Service Area</h5>
+                <hr>
+            </div>
+        </div>
+
+        <div class="row row-cols-lg-4 row-cols-md-2 row-cols-sm-1 justify-content-center">
+            <div class="col service-area">
+                <div>
+                    <img src="Image/dhaka.jpg" alt="dhaka">
+                    <h6>Dhaka</h6>
+                </div>
+            </div>
+            <div class="col service-area">
+                <div>
+                    <img src="Image/chattogram.jpg" alt="dhaka">
+                    <h6>Chattogram</h6>
+                </div>
+            </div>
+            <div class="col service-area">
+                <div>
+                    <img src="Image/sylhet.jfif" alt="dhaka">
+                    <h6>Sylhet</h6>
+                </div>
+            </div>
+            <div class="col service-area">
+                <div>
+                    <img src="Image/barishal.jpg" alt="dhaka">
+                    <h6>Barishal</h6>
+                </div>
+            </div>
+            <div class="col service-area">
+                <div>
+                    <img src="Image/rajshahi.webp" alt="dhaka">
+                    <h6>Rajshahi</h6>
+                </div>
+            </div>
+            <div class="col service-area">
+                <div>
+                    <img src="Image/khulna.jfif" alt="dhaka">
+                    <h6>Khulna</h6>
+                </div>
+            </div>
+            <div class="col service-area">
+                <div>
+                    <img src="Image/mymensingh.webp" alt="dhaka">
+                    <h6>Mymensingh </h6>
+                </div>
+            </div>
+            <div class="col service-area">
+                <div>
+                    <img src="Image/rangpur.webp" alt="dhaka">
+                    <h6>Rangpur</h6>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row row-cols-3 justify-content-center">
+            <div class="col section-header">
+                <h5>How it works</h5>
+                <hr>
+            </div>
+        </div>
+
+        <div class="row row-cols-lg-3 row-cols-md-1 row-cols-sm-1 justify-content-center">
+            <div class="col step">
+                <h5>Step 1:</h5><br>
+                <img src="Image/create.svg" alt="create"><br><br>
+                <h6>Create your Account</h6>
+            </div>
+            <div class="col step">
+                <h5>Step 2:</h5><br>
+                <img src="Image/search.svg" alt="create"><br><br>
+                <h6>Find Service</h6>
+            </div>
+            <div class="col step">
+                <h5>Step 3:</h5><br>
+                <img src="Image/done.svg" alt="create"><br><br>
+                <h6>All Done!</h6>
+            </div>
+        </div>
+
+
+
+        <div class="row row-cols-3 justify-content-center">
+            <div class="col section-header">
+                <h5>Customer Services</h5>
+                <hr>
+            </div>
+        </div>
+        <div class="row row-cols-lg-2 row-cols-md-1 row-cols-sm-1 justify-content-center align-items-center" >
+            <div class="col care justify-content-left">
+                <form class="care-form" action="#" method="Post">
+                    <label for="name">Name:</label>
+                    <input type="text" placeholder="Enter Your Name*" name="name" required><br>
+                    <label for="email">Email:</label>
+                    <input type="email" placeholder="Your Contact Mail*" name="email" required><br>
+                    <label for="suject">Subject:</label>
+                    <input type="text" placeholder="Subject of query*" name="subject" required><br>
+                    <label for="message">Message:</label>
+                    <textarea name="message" placeholder="Please enter you query/suggestions*" cols="20" rows="4" required></textarea><br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+            <div class="col care">
+                <img src="Image/care.jpg" alt=" Customer Care">
+            </div>
+        </div>
+
+
+
+
+
+    </main>
 
 
 
 
 
 
-    <div style="height: 100vh; background-color: aqua">
+
+
+
+
+
+
+    <!-- <div style="height: 100vh; background-color: aqua">
         <h1>Body start</h1>
         <br>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi sequi fugiat id non, quos dolor odit
@@ -167,7 +322,7 @@ if(isset($_SESSION['loginTime']))
             cumque aspernatur vitae neque quia odit rerum. Suscipit placeat ad illo expedita optio laudantium eaque
             ullam.
         </p>
-    </div>
+    </div> -->
 
 
 
@@ -178,7 +333,9 @@ if(isset($_SESSION['loginTime']))
     <footer class="footer">
         <div class="footer-left">
             <img src="Image/Logo.png" alt="" style="width: 70px; margin-left: 30px; margin-right: 20px;"><br>
-            <span><p style="font-weight: bolder; font-size: large; margin-left: 10px;"> Medi Sol</p></span><br><br>
+            <span>
+                <p style="font-weight: bolder; font-size: large; margin-left: 10px;"> Medi Sol</p>
+            </span><br><br>
             <p>Health is a right, not a privilege. It needs to be enjoyed with equity.</p>
             <div class="socials">
                 <a href="#"><i class="fa-brands fa-facebook"></i></a>
@@ -186,8 +343,8 @@ if(isset($_SESSION['loginTime']))
                 <a href="#"><i class="fa-brands fa-youtube"></i></a>
                 <a href="#"><i class="fa-brands fa-tumblr"></i></a>
                 <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                
-                
+
+
             </div>
         </div>
 
@@ -234,7 +391,7 @@ if(isset($_SESSION['loginTime']))
 
 
 
-    
+
 
 
 
@@ -246,15 +403,9 @@ if(isset($_SESSION['loginTime']))
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
